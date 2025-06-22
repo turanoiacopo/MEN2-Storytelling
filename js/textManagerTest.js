f63b0d4abdb38482bb654a548bb027c1c7008050
turanoiacopo <iacopo.turano@stud.hslu.ch>
2025-06-22 22:33:24 +0200

    Testing scene manager

--- js/textManagerTest.js
+++ js/textManagerTest.js
@@ -22,3 +22,47 @@ function language_manager()
             break;
     }
 }
+
+function scene_manager(scene)
+{
+    switch(scene) {
+        case "premise":
+            break;
+        case "conclusion":
+            break;
+        case "rectangles":
+            break;
+        case "senses":
+            break;
+        case "remember":
+            break;
+        default:
+            console.log('scene manager error');
+            break;
+    }
+}
+
+function scene_premise()
+{
+    scene_manager("premise");
+}
+
+function scene_conclusion()
+{
+    scene_manager("conclusion");
+}
+
+function scene_remember()
+{
+    scene_manager("remember");
+}
+
+function scene_senses()
+{
+    scene_manager("senses");
+}
+
+function scene_rectangles()
+{
+    scene_manager("rectangles");
+}
