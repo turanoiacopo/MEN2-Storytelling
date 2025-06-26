78d4f7f0d5de7b9d6ddd69dad7318d2b8f944d8e
turanoiacopo <iacopo.turano@stud.hslu.ch>
2025-06-26 22:13:04 +0200

    Creation of scene structure

--- js/textManagerTest.js
+++ js/textManagerTest.js
@@ -1,13 +1,31 @@
+////////////////////////////////////////////////
+// DOM elements
+
 const myBody = document.getElementById('body');
 const language = document.getElementsByTagName('html')[0].attributes.lang.value;
+const scene = document.getElementById('scene');
+const grid = document.getElementById('parent-container');
+
+////////////////////////////////////////////////
+// Global variables
 
 let texts = 0;
 
+////////////////////////////////////////////////
+// Main
+
+// Language selection
 language_manager();
 
-myBody.innerHTML = texts.premise.body;
+// Scene initialization
+scene_premise();
 
+////////////////////////////////////////////////
+// Functions
+
+//---------------------------------------------
 function language_manager()
+//---------------------------------------------
 {
     switch(language) {
         case 'en':
@@ -23,7 +41,9 @@ function language_manager()
     }
 }
 
+//---------------------------------------------
 function scene_manager(scene)
+//---------------------------------------------
 {
     switch(scene) {
         case "premise":
@@ -42,27 +62,58 @@ function scene_manager(scene)
     }
 }
 
+//---------------------------------------------
 function scene_premise()
+//---------------------------------------------
 {
-    scene_manager("premise");
+    const body = document.createElement('p');
+    const rememberButton = document.createElement('button');
+    
+    scene.append(body);
+    scene.append(rememberButton);
+
+    rememberButton.innerHTML = "Remember";
+
+    grid.style.gridTemplateColumns = "1fr 3fr 1fr";
+
+    body.innerHTML = texts.premise.body;
+
+    rememberButton.addEventListener("click", () => {
+        body.remove();
+        rememberButton.remove();
+
+        scene_remember();
+    });
 }
 
+//---------------------------------------------
 function scene_conclusion()
+//---------------------------------------------
 {
     scene_manager("conclusion");
 }
 
+//---------------------------------------------
 function scene_remember()
+//---------------------------------------------
 {
+    const body = document.createElement('p');
+    scene.append(body);
+
+    body.innerHTML = "ciao"
     scene_manager("remember");
 }
 
+//---------------------------------------------
 function scene_senses()
+//---------------------------------------------
 {
     scene_manager("senses");
 }
 
+//---------------------------------------------
 function scene_rectangles()
+//---------------------------------------------
 {
     scene_manager("rectangles");
 }
--- resources/texts/texts_en.js
+++ resources/texts/texts_en.js
@@ -1,7 +1,7 @@
 const textsEN = {
     "premise": {
         "title": "Premise",
-        "body": "Test EN",
+        "body": "My cell is 71.52 light-years away from planet Earth. It orbits around the black hole RS 8513-928-8-5906646-198, where I was sent for reasons I no longer remember. This is what I know about myself: the entire front of my body from the neck down, and the back of my legs.<br /> When I feel hungry, I eat a ration of food.<br /> When I feel thirsty, I drink a ration of water.<br /> When I feel bored, I read one of the books on the shelf in my cell.<br /> When I am neither hungry nor thirsty nor bored, I do nothing.<br /> A thought unsettles me. Since I no longer know the fate of the human species, it would be unrealistic to rule out the possibility that I am the last human alive. For some reason, I feel the need to remember my past in order to preserve it for as long as possible.<br /> From now on, when I am neither hungry nor thirsty nor bored, I will try to remember who I am.",
     },
 
     "conclusion": {
